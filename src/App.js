f4d4da8fbb961d0ab0e607fb8debd3f4783e010a
Karishma <karishmamhatre077@gmail.com>
2018-10-18 22:44:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: {
+        results: [
+          {
+            
+          }
+        ]
+      }
+    }
+  }
+  componentDidMount() {
+    var oxfordCreds = {
+      apiId: "b5af5ac6",
+      apiKey: "1ffc6a29363ac7760e8c8bca3ebb59a4"
+    }
+    
+    fetch('https://od-api.oxforddictionaries.com/api/v1/entries/en/ace/regions=us',{
+      method: 'GET',
+      headers: new Headers({
+        'app_id': oxfordCreds.apiId, 'app_key': oxfordCreds.apiKey
+      }),
+    }).then((response) => {
+        console.log(response)
+        this.setState({response: response});
+      }, (error) => {
+        console.log(error);
+      });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>{this.state.response.results[0].id}</div>
       </div>
     );
   }
